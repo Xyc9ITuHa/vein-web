5183ef4bf2532c10a7c7f0d4dd8065d33a3517e0
Xyc| Artem K. <zoom.2000v@gmail.com>
2025-08-17 17:32:19 +0300

    comment out for perfomance test

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,16 +1,16 @@
 import { lazy, useEffect, useRef } from "react";
 import { LogoColors } from "../icons/LogoColors.tsx";
-import { animate, createScope, onScroll, utils } from "animejs";
+{/*import { animate, createScope, onScroll, utils } from "animejs";
 const ring_xl = "/images/ring_xl.webp";
 const ring_lg = "/images/ring_lg.webp";
 const ring_md = "/images/ring_md.webp";
-const ring_sm = "/images/ring_sm.webp";
+const ring_sm = "/images/ring_sm.webp";*/}
 const ParallaxDiv = lazy(() => import("../common/ParalaxDiv.tsx"));
 
 
 
 function Hero() {
-    const root = useRef(null);
+    {/*    const root = useRef(null);
     const scope = useRef<ReturnType<typeof createScope> | undefined>(null)
 
     useEffect(() => {
@@ -38,23 +38,24 @@ function Hero() {
 
 
         return () => { scope.current && scope.current.revert(); };
-    }, []);
+
+    }, []);*/}
 
     return (
         <>
             <section id="hero" className="relative">
-                <div ref={root} className="relative min-h-screen flex flex-col hero-container">
+                <div className="relative min-h-screen flex flex-col hero-container">
 
 
 
                     {/* Main content */}
                     <div className="flex-1 flex flex-col items-center justify-center text-center px-4 py-8 sm:px-6 lg:px-8">
-                        <div className="absolute -z-70 inset-0 overflow-hidden bg-gradient-to-bl from-cream to-accent">
+                        {/*<div className="absolute -z-70 inset-0 overflow-hidden bg-gradient-to-bl from-cream to-accent">
                             <img src={ring_sm} id="ring" alt="small ring from flowers photo" className="absolute opacity-15 sepia brightness-200" />
                             <img src={ring_md} id="ring" alt="medium ring from flowers photo" className="absolute opacity-12 sepia-90 brightness-175" />
                             <img src={ring_lg} id="ring" alt="large ring from flowers photo" className="absolute opacity-9 sepia-80 brightness-150" />
                             <img src={ring_xl} id="ring" alt="very large ring from flowers photo" className="absolute opacity-6 sepia-70 brightness-100" />
-                        </div>
+                        </div>*/}
                         <ParallaxDiv className="-z-60 relative flex flex-col items-center">
                             <LogoColors className="h-52 w-auto md:h-60 lg:h-70 xl:h-80" />
                             <h1 className="font-dune text-6xl lg:text-7xl xl:text-8xl text-secondary mt-2 sm:mt-3">
